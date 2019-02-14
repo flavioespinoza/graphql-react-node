7fe4c1da63ec104a16a228c40db1e672318dd065
Flavio Espinoza <flavio.espinoza@gmail.com>
2019-02-14 03:16:14 -0700

    Cleaned up Create Reservation method.

--- client/src/components/CreateReservation.js
+++ client/src/components/CreateReservation.js
@@ -4,42 +4,22 @@ import gql from 'graphql-tag'
 import { Mutation } from 'react-apollo'
 import { graphql } from 'react-apollo'
 import axios from 'axios'
+import Chance from 'chance'
 
+const chance = new Chance()
 
-const newReservation = (name, hotelName, arrivalDate, departureDate) => {
-    return `
-        mutation {
-            createReservation(
-                data: {
-                    name: ${name}
-                    hotelName: ${hotelName}
-                    arrivalDate: ${arrivalDate}
-                    departureDate: ${departureDate}
-                }
-            ) {
-                id
-                name
-                hotelName
-                arrivalDate
-                departureDate
-            }
-        }
-    `
-}
 
 class CreateReservation extends React.Component {
 	constructor (props) {
 		super(props)
 
-        this._postReservation = this._postReservation.bind(this)
+        this._createReservation = this._createReservation.bind(this)
 
-		this.state = {
-
-		}
+		this.state = {}
 
 	};
 
-	_postReservation () {
+	_createReservation (firstName, lastName, hotelName, arrivalDate, departureDate) {
 
         const _endpoint = 'https://us1.prisma.sh/public-luckox-377/reservation-graphql-backend/dev'
 
@@ -51,10 +31,10 @@ class CreateReservation extends React.Component {
                     mutation {
                         createReservation(
                         data: {
-                            name: "Ren"
-                            hotelName: "Hilton NYC"
-                            arrivalDate: "2/28/19"
-                            departureDate: "3/12/19"
+                            name: "${firstName} ${lastName}"
+                            hotelName: "${hotelName}"
+                            arrivalDate: "${arrivalDate}"
+                            departureDate: "${departureDate}"
                         }
                         ) {
                             id
@@ -78,9 +58,9 @@ class CreateReservation extends React.Component {
 			<section>
 				
                 <PrimaryButton
-                    data-automation-id="stuff"
-                    text="Post Shit"
-                    onClick={this._postReservation}
+                    data-automation-id="Reservation"
+                    text="Create Reservation"
+                    onClick={() => {this._createReservation(chance.first(), chance.last(), 'Hilton NYC', '5/24/19', '6/24/19')}}
                 />
 
 
